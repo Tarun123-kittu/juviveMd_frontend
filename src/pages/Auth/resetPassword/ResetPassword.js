4db6a81727a5f3e2090b95abcafb474ef55f83fa
deepak chauhan <deepak.chauhan@ultivic.com>
2024-11-28 10:56:38 +0530

    reset password page

--- src/pages/Auth/resetPassword/ResetPassword.js
+++ src/pages/Auth/resetPassword/ResetPassword.js
@@ -4,7 +4,9 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { reset_password,clear_reset_password_state } from "../../../redux/slices/authSlice/resetPasswordSlice";
 import { useNavigate } from "react-router-dom";
-
+import Form from "react-bootstrap/Form";
+import Logo from '../../../Images/juviveLogo.svg'
+import './ResetPassword.css'
 const ResetPassword = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate()
@@ -41,18 +43,36 @@ const ResetPassword = () => {
     }
   }, [reset_password_state]);
   return (
-    <div>
-      <input
-        type="text"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input
-        type="text"
-        value={confirm_password}
-        onChange={(e) => setConfirm_password(e.target.value)}
-      />
-      <button onClick={(e) => handleChangePassword(e)}>Change Password</button>
+    <div className="reset_wrapper min-vh-100 d-flex align-items-center justify-content-center">
+      <div className="reset_form authWrapper ">
+        <div className="text-center">
+        <img src={Logo} alt="logo" />
+        </div>
+        <h3 className="mb-1">Create Password</h3>
+        <h5>Create your password for your account so you can login and access all features.</h5>
+      <Form.Group className="mb-2" controlId="exampleForm.ControlInput1">
+            <Form.Label>Create new password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your new password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-2" controlId="exampleForm.ControlInput1">
+            <Form.Label>Confirm password</Form.Label>
+            <Form.Control
+              placeholder="Confirm your password"
+              type="text"
+              value={confirm_password}
+              onChange={(e) => setConfirm_password(e.target.value)}
+            />
+          </Form.Group>
+    
+                <div className="mt-3">
+                <button className="cmn_btn w-100" onClick={(e) => handleChangePassword(e)}>Change Password</button>
+                </div>
+      </div>
     </div>
   );
 };
